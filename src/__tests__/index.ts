ee49d5e95702c9bfad38b3d8144802991d2a792d
rchaser53 <tayoshizawa29@gmail.com>
2018-02-10 14:49:51 +0900

    add empty case test

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,16 +1,28 @@
 import parse5 = require('parse5')
-import { getTarget } from '../index'
+import {
+  getTarget
+} from '../index'
 
 describe("getTarget", () => {
-  test("return script tag", async () => {
+  test("return second parameter matched tag", async () => {
     const hlmlString = `<html>
       <head>
         <script></script>
       <head>
     </html>`
     const ret = getTarget(parse5.parse(hlmlString), 'script')
-    expect(ret.length).toEqual(1);
-    expect(ret[0].nodeName).toEqual('script');
+    expect(ret.length).toBe(1);
+    expect(ret[0].nodeName).toBe('script');
+  })
 
+  test("return empty array if not matched", async () => {
+    const hlmlString = `<html>
+      <head>
+        <script></script>
+      <head>
+    </html>`
+    const ret = getTarget(parse5.parse(hlmlString), 'div')
+    expect(ret.length).toBe(0);
+    expect(ret).toEqual([]);
   })
 })
