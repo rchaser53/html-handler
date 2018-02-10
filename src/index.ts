eb1cd37a911a2b741d09d4eb9b8c09b41cdc24ba
rchaser53 <tayoshizawa29@gmail.com>
2018-02-10 14:22:21 +0900

    add temp type for this implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-export const getTarget = (targetNode: any, key: string, retArray: any[] = []): any[] => {
+// it's not correct type. just for this implements
+export interface ParsedNode {
+  attrs: { name: string, value: any },
+  childNodes: ParsedNode[],
+  namespaceURI: string,
+  nodeName: string,
+  parentNode: ParsedNode,
+  tagName: string,
+}
+
+export const getTarget = (targetNode: ParsedNode, key: string, retArray: ParsedNode[] = []): ParsedNode[] => {
   targetNode.childNodes.forEach((node: any) => {
     if (node.tagName === key) {
       retArray.push(node)
