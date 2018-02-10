ef005b485ace79f184f17da0c415355c9cd2bbaf
rchaser53 <tayoshizawa29@gmail.com>
2018-02-10 17:00:55 +0900

    apply attribute for second parameter

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,40 +1,58 @@
 import parse5 = require('parse5')
-import {
-  getTarget
-} from '../index'
+import { getTarget } from '../index'
 
-describe("getTarget", () => {
-  test("return matched tag", async () => {
-    const hlmlString = `<html>
+describe('getTarget', () => {
+	test('return matched tag', async () => {
+		const hlmlString = `<html>
       <head>
         <script></script>
-      <head>
+      </head>
     </html>`
-    const ret = getTarget(parse5.parse(hlmlString), 'script')
-    expect(ret.length).toBe(1);
-    expect(ret[0].nodeName).toBe('script');
-  })
+		const ret = getTarget(parse5.parse(hlmlString), 'script')
+		expect(ret.length).toBe(1)
+		expect(ret[0].nodeName).toBe('script')
+	})
 
-  test("return empty array if not matched", async () => {
-    const hlmlString = `<html>
+	test('return empty array if not matched', async () => {
+		const hlmlString = `<html>
       <head>
         <script></script>
-      <head>
+      </head>
     </html>`
-    const ret = getTarget(parse5.parse(hlmlString), 'div')
-    expect(ret.length).toBe(0);
-    expect(ret).toEqual([]);
-  })
+		const ret = getTarget(parse5.parse(hlmlString), 'div')
+		expect(ret.length).toBe(0)
+		expect(ret).toEqual([])
+	})
 
-  test("decides the condition for returning tag", async () => {
-    const hlmlString = `<html>
+	test('return node which tag name is script', async () => {
+		const hlmlString = `<html>
       <head>
         <script></script>
-      <head>
+      </head>
     </html>`
 
-    const ret = getTarget(parse5.parse(hlmlString), { type: 'tag', value: 'script' })
-    expect(ret.length).toBe(1);
-    expect(ret[0].nodeName).toBe('script');
-  })
+		const ret = getTarget(parse5.parse(hlmlString), { type: 'tag', value: 'script' })
+		expect(ret.length).toBe(1)
+		expect(ret[0].nodeName).toBe('script')
+	})
+
+	test("return node which attribute src is 'js/hoge.js'", async () => {
+		const hlmlString = `<html>
+      <head>
+        <script src="js/hoge.js"></script>
+        <script src="js/fuga.js"></script>
+        <link href="test"></link>
+      </head>
+    </html>`
+
+		const ret = getTarget(parse5.parse(hlmlString), {
+			type: 'attribute',
+			value: {
+				name: 'src',
+				value: 'js/hoge.js'
+			}
+		})
+		expect(ret.length).toBe(1)
+		expect(ret[0].nodeName).toBe('script')
+	})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // it's not correct type. just for this implements
 export interface ParsedNode {
-  attrs: Attribute,
-  childNodes: ParsedNode[],
-  namespaceURI: string,
-  nodeName: string,
-  parentNode: ParsedNode,
-  tagName ? : string,
+	attrs: Attribute[]
+	childNodes: ParsedNode[]
+	namespaceURI: string
+	nodeName: string
+	parentNode: ParsedNode
+	tagName?: string
 }
 
 export interface Attribute {
-  name: string,
-  value: any
+	name: string
+	value: any
 }
 
 export type Primitive = string | number | boolean | null
@@ -18,54 +18,81 @@ export type OptionType = 'tag' | 'node' | 'attribute'
 export type ParsedNodeKey = 'attrs' | 'nodeName' | 'tagName'
 
 export interface Option {
-  type: 'tag' | 'node' | 'attribute'
-  value: Primitive | Attribute
+	type: 'tag' | 'node' | 'attribute'
+	value: Primitive | Attribute
+}
+
+export interface AttributeOption {
+	type: 'attribute'
+	value: Attribute
 }
 
 // just wanna decrease the parameters for developer
 export const getTarget = (targetNode: ParsedNode, option: Option | string): ParsedNode[] => {
-  let returnArray: ParsedNode[] = []
-  searchNode(targetNode, option, returnArray)
-  return returnArray
+	let returnArray: ParsedNode[] = []
+	searchNode(targetNode, option, returnArray)
+	return returnArray
 }
 
-export const recursiveGetTarget = (targetNode: ParsedNode, option: Option | string, targetArray: ParsedNode[]): ParsedNode[] => {
-  searchNode(targetNode, option, targetArray)
-  return targetArray
+export const recursiveGetTarget = (
+	targetNode: ParsedNode,
+	option: Option | string,
+	targetArray: ParsedNode[]
+): ParsedNode[] => {
+	searchNode(targetNode, option, targetArray)
+	return targetArray
 }
 
 const isOption = (option: Option | string): option is Option => {
-  return (<Option>option).type !== undefined
+	return (<Option>option).type !== undefined
+}
+
+const isAttribute = (option: Option | AttributeOption): option is AttributeOption => {
+	return option.type === 'attribute'
 }
 
 const convertTypeToKey = (type: OptionType): ParsedNodeKey => {
-  switch (type) {
-    case 'tag':
-      return 'tagName'
-    case 'node':
-      return 'nodeName'
-    case 'attribute':
-      return 'attrs'
-  }
+	switch (type) {
+		case 'tag':
+			return 'tagName'
+		case 'node':
+			return 'nodeName'
+		case 'attribute':
+			return 'attrs'
+	}
+}
+
+const hasMatchedAttribute = (node: ParsedNode, option: Attribute): boolean => {
+	return (
+		!!node.attrs &&
+		node.attrs.some((attribute) => {
+			return attribute.name === option.name && attribute.value === option.value
+		})
+	)
 }
 
 const searchNode = (targetNode: ParsedNode, option: Option | string, targetArray: ParsedNode[]) => {
-  targetNode.childNodes.forEach((node: ParsedNode) => {
-    if (typeof option === 'string') {
-      if (node.tagName === option) {
-        targetArray.push(node)
-      }
-    }
+	targetNode.childNodes.forEach((node: ParsedNode) => {
+		if (typeof option === 'string') {
+			if (node.tagName === option) {
+				targetArray.push(node)
+			}
+		}
 
-    if (isOption(option)) {
-      const key = convertTypeToKey(option.type)
-      if (node[key] === option.value) {
-        targetArray.push(node)
-      }
-    }
+		if (isOption(option)) {
+			const key = convertTypeToKey(option.type)
 
-    if (!!node.childNodes && 0 < node.childNodes.length) {
-      recursiveGetTarget(node, option, targetArray)
-    }
-  })
+			if (isAttribute(option)) {
+				if (hasMatchedAttribute(node, option.value)) {
+					targetArray.push(node)
+				}
+			} else if (node[key] === option.value) {
+				targetArray.push(node)
+			}
+		}
+
+		if (!!node.childNodes && 0 < node.childNodes.length) {
+			recursiveGetTarget(node, option, targetArray)
+		}
+	})
 }
