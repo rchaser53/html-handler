ee94d9f6f19dcd6ee27e498c762a5cab32991226
rchaser53 <tayoshizawa29@gmail.com>
2018-02-10 15:37:20 +0900

    change seconde parameter type

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,47 @@
 // it's not correct type. just for this implements
 export interface ParsedNode {
-  attrs: {
-      name: string,
-      value: any
-    },
-    childNodes: ParsedNode[],
-    namespaceURI: string,
-    nodeName: string,
-    parentNode: ParsedNode,
-    tagName?: string,
+  attrs: Attribute,
+  childNodes: ParsedNode[],
+  namespaceURI: string,
+  nodeName: string,
+  parentNode: ParsedNode,
+  tagName ? : string,
+}
+
+export interface Attribute {
+  name: string,
+  value: any
+}
+
+export type Primitive = string | number | boolean | null
+
+export interface Option {
+  type: 'tag' | 'node' | 'attribute'
+  value: Primitive | Attribute
 }
 
 // just wanna decrease the parameters for developer
-export const getTarget = (targetNode: ParsedNode, key: string): ParsedNode[] => {
+export const getTarget = (targetNode: ParsedNode, option: Option | string): ParsedNode[] => {
   let returnArray: ParsedNode[] = []
-  searchNode(targetNode, key, returnArray)
+  searchNode(targetNode, option, returnArray)
   return returnArray
 }
 
-export const recursiveGetTarget = (targetNode: ParsedNode, key: string, targetArray: ParsedNode[]): ParsedNode[] => {
-  searchNode(targetNode, key, targetArray)
+export const recursiveGetTarget = (targetNode: ParsedNode, option: Option | string, targetArray: ParsedNode[]): ParsedNode[] => {
+  searchNode(targetNode, option, targetArray)
   return targetArray
 }
 
-const searchNode = (targetNode: ParsedNode, key: string, targetArray: ParsedNode[]) => {
+const searchNode = (targetNode: ParsedNode, option: Option | string, targetArray: ParsedNode[]) => {
   targetNode.childNodes.forEach((node: ParsedNode) => {
-    if (node.tagName === key) {
-      targetArray.push(node)
-    } else if (!!node.childNodes && 0 < node.childNodes.length) {
-      recursiveGetTarget(node, key, targetArray)
+    if (typeof option === 'string') {
+      if (node.tagName === option) {
+        targetArray.push(node)
+      }
+    }
+
+    if (!!node.childNodes && 0 < node.childNodes.length) {
+      recursiveGetTarget(node, option, targetArray)
     }
   })
 }
