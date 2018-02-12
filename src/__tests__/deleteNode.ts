e69f9372cffe38a4ea90b31523ddd6d6d6ec90e7
rchaser53 <tayoshizawa29@gmail.com>
2018-02-12 10:48:01 +0900

    add test for deleting nest node
    parse5 has the bug cannot delete child node completely at some case

--- src/__tests__/deleteNode.ts
+++ src/__tests__/deleteNode.ts
@@ -23,4 +23,30 @@ describe('deleteNode', () => {
 		deleteNode(document, 'div')
 		expect(trimWhiteSpace(parse5.serialize(document))).toBe(trimWhiteSpace(expectedHtmlString))
 	})
+
+	test('delete arbitrary nest node', async () => {
+		const inputHlmlString = `
+<html>
+  <head></head>
+  <body>
+    <table>
+      <tbody>
+        <tr>
+          <td>hoge</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
+`
+		const expectedHtmlString = `
+<html>
+  <head></head>
+  <body></body>
+</html>
+`
+		const document = parse5.parse(inputHlmlString)
+		deleteNode(document, 'table')
+		expect(trimWhiteSpace(parse5.serialize(document))).toBe(trimWhiteSpace(expectedHtmlString))
+	})
 })
